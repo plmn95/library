18a73bb2e658e87d59ef2a2bec6c0171f8fa41a4
plmn95 <plmn9595@gmail.com>
2025-01-13 01:19:13 +0200

    i cant believe it works

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function Book(title, author, pages, haveRead) {
     this.haveRead = haveRead
 }
 
-const holyland = new Book('Holyland', 'Kouji Mori', '212', true)
 const braveNewWorld = new Book('Brave New World', 'Aldous Huxley', '288', true)
+const nightInLisbon = new Book('The Night in Lisbon', 'Erich Maria Remarque', '272', true)
 
-const myLibrary = [holyland, braveNewWorld]
+const myLibrary = [braveNewWorld, nightInLisbon]
 
 function addToLibrary(book) {
     myLibrary.push(book)
@@ -26,32 +26,86 @@ function displayLibrary(books) {
         let title = document.createElement('p')
         title.classList.add('title')
         title.innerText = `${books[i].title}`
+        let titleEdit = document.createElement('input')
+        titleEdit.classList.add('title')
+        titleEdit.type = 'text'
         book.appendChild(title)
         let author = document.createElement('p')
         author.classList.add('author')
         author.innerText = `${books[i].author}`
+        let authorEdit = document.createElement('input')
+        authorEdit.classList.add('author')
+        authorEdit.type = 'text'
         book.appendChild(author)
         let pages = document.createElement('p')
-        pages.classList.add('author')
-        pages.innerText = `${books[i].pages} pages long`
+        pages.classList.add('pages')
+        pages.innerText = `${books[i].pages}`
+        let pagesEdit = document.createElement('input')
+        pagesEdit.type = 'text'
+        pagesEdit.classList.add('pages')
         book.appendChild(pages)
-        let haveRead = document.createElement('input')
-        haveRead.type = 'checkbox'
-        haveRead.classList.add('haveRead')
-        haveRead.addEventListener('click', () => {
-            books[i].haveRead = haveRead.checked
-        })
+        let haveRead = document.createElement('p')
+        haveRead.classList.add('btnHaveRead')
+        haveRead.classList.add('btn')
         if(books[i].haveRead == true) {
-            haveRead.checked = true
+            haveRead.innerText = 'read'
+            haveRead.classList.add('haveReadTrue')
         } else {
-            haveRead.checked = false
+            haveRead.innerText = 'unread'
+            haveRead.classList.add('haveReadFalse')
         }
         book.appendChild(haveRead)
+        haveRead.addEventListener('click', () => {
+            if(books[i].haveRead == true) {
+                books[i].haveRead = false
+                haveRead.innerText = 'unread'
+                haveRead.classList.remove('haveReadTrue')
+                haveRead.classList.add('haveReadFalse')
+            } else {
+                books[i].haveRead = true
+                haveRead.innerText = 'read'
+                haveRead.classList.remove('haveReadFalse')
+                haveRead.classList.add('haveReadTrue')
+            }
+        })
+        let btnEdit = document.createElement('p')
+        btnEdit.innerText = 'edit'
+        btnEdit.classList.add('btnEdit')
+        btnEdit.classList.add('btn')
+        btnEdit.addEventListener('click', () => {
+            if(btnEdit.innerText == 'edit') {
+                btnEdit.innerText = 'save'
+                titleEdit.value = books[i].title
+                title.parentNode.replaceChild(titleEdit, title)
+                authorEdit.value = books[i].author
+                author.parentNode.replaceChild(authorEdit, author)
+                pagesEdit.value = books[i].pages
+                pages.parentNode.replaceChild(pagesEdit, pages)
+            } else {
+                if(titleEdit.value != '') {
+                    books[i].title = titleEdit.value
+                    title.innerText = books[i].title
+                }
+                titleEdit.parentNode.replaceChild(title, titleEdit)
+                if(authorEdit.value != '') {
+                    books[i].author = authorEdit.value
+                    author.innerText = books[i].author
+                }
+                authorEdit.parentNode.replaceChild(author, authorEdit)
+                if(pagesEdit.value != '') {
+                    books[i].pages = pagesEdit.value
+                    pages.innerText = books[i].pages
+                }
+                pagesEdit.parentNode.replaceChild(pages, pagesEdit)
+                btnEdit.innerText = 'edit'
+            }
+        })
+        book.appendChild(btnEdit)
         let btnRemove = document.createElement('p')
         btnRemove.innerText = 'delete'
         btnRemove.classList.add('btnRemove')
+        btnRemove.classList.add('btn')
         btnRemove.addEventListener('click', () => {
-            console.log(book.id)
             myLibrary.splice(i, 1)
             clearLibrary()
             displayLibrary(myLibrary)
@@ -78,20 +132,39 @@ const btnAdd = document.querySelector('.btnAdd')
 const addBookDialog = document.querySelector('dialog')
 const btnCloseModal = document.querySelector('.btnCloseModal')
 const btnSubmit = document.querySelector('.btnSubmit')
+const btnFormRead = document.querySelector('.btnFormRead')
+const btnFormUnread = document.querySelector('.btnFormUnread')
 
 btnAdd.addEventListener('click', () => {
     addBookDialog.showModal()
-    // addBook('jumanji', 'text', '500', false)
 })
 
 btnCloseModal.addEventListener('click', () => {
     addBookDialog.close()
 })
 
+btnFormRead.addEventListener('click', () => {
+    if(btnFormUnread.classList.contains('btnHaveReadSelected')) {
+        btnFormUnread.classList.remove('btnHaveReadSelected')
+        btnFormRead.classList.add('btnHaveReadSelected')
+    }
+})
+
+btnFormUnread.addEventListener('click', () => {
+    if(btnFormRead.classList.contains('btnHaveReadSelected')) {
+        btnFormRead.classList.remove('btnHaveReadSelected')
+        btnFormUnread.classList.add('btnHaveReadSelected')
+    }
+})
+
 btnSubmit.addEventListener('click', () => {
     const name = document.querySelector('.inputName').value
     const author = document.querySelector('.inputAuthor').value
     const pages = document.querySelector('.inputPages').value
-    const haveRead = document.querySelector('.inputHaveRead').checked
+    let haveRead = true
+    if(btnFormUnread.classList.contains('btnHaveReadSelected')) {
+        haveRead = false
+    }
     addBook(name, author, pages, haveRead)
+    addBookDialog.close()
 })
